b8812416e85a8d9c53b48aeb61df711fcb3f2378
Supreeth1311 <76720710+Supreeth1311@users.noreply.github.com>
2021-07-13 17:06:02 +0530

    Add files via upload

---
+++ code/test.py
@@ -0,0 +1,10 @@
+f = open("assets/input_sudoku board.txt", "r")
+message = f.read()
+
+grid = []
+unsolved_grid = []
+k = message.split('\n')
+for line in k:
+    m = line.split(" ")
+    l = list(map(int , m))
+    grid.append(l)
